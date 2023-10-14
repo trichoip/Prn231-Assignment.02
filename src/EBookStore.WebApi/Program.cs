72a339d70ae8ae7053fa151dec0402351a80054d
trichoip <107303342+trichoip@users.noreply.github.com>
2023-10-14 10:54:55 +0700

    Update Program.cs

--- src/EBookStore.WebApi/Program.cs
+++ src/EBookStore.WebApi/Program.cs
@@ -46,3 +46,10 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+// https://localhost:7089/odata/Users?$top=1&$orderby=HireDate desc&$expand=Publisher,Role($top=1;$orderby=RoleId desc)
+// https://localhost:7089/odata/Users?$filter=hour(HireDate) ge 18 or hour(HireDate) lt 6&$orderby=HireDate desc&$top=5&$expand=Publisher
+// https://localhost:7089/odata/Publishers?$orderby=Books/$count desc&$expand=Books($top=5;$orderby=Price desc)
+// https://localhost:7089/odata/Books?$expand=BookAuthors($expand=Author,Book)&$filter=PublishedDate ge 2015-01-01&$orderby=PublishedDate desc
+// https://localhost:7089/odata/Authors?$expand=BookAuthors($expand=Book)&$filter=BookAuthors/$count gt 0&$orderby=AuthorId desc
